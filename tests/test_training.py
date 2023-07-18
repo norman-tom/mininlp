e5a34bc903fcf9128d1acc7f356a53a78254cc09
norman-tom <tom.a.norman@gmail.com>
2023-07-18 17:53:28 +1000

    updated to give tokens

--- tests/test_training.py
+++ tests/test_training.py
@@ -19,20 +19,29 @@ class TestTraining(unittest.TestCase):
         mask = torch.triu(torch.ones(max_seq, max_seq), diagonal=1)
         model = DTransformer(N, de, v_size, max_seq, n_heads, factor, mask)
 
+    def test_simple_train(self) -> None:
+        max_seq = 10
+        de = 24
+        v_size = 26
+        n_heads = 4
+        N = 2
+        factor = 4
+        mask = torch.triu(torch.ones(max_seq, max_seq), diagonal=1)
+        model = DTransformer(N, de, v_size, max_seq, n_heads, factor, mask)
+
         class Data(Dataset):
             def __init__(self, vocab_size, n_examples, l_seq) -> None:
                 super().__init__()
-                self._data = torch.randint(0, vocab_size, (n_examples, l_seq+1))
+                self._data = torch.randint(0, vocab_size, (n_examples,))
                 self._vocab_size = vocab_size
                 self._seq = l_seq
 
-            def __len__(self) -> int:
-                return self._data.size()[0]
-            
+            def __len__(self):
+                return len(self._data) - self._seq
+        
             def __getitem__(self, index) -> tuple[torch.Tensor, torch.Tensor]:
-                x = self._data[index][:-1]
-                y = F.one_hot(self._data[index][1:], num_classes=self._vocab_size)
-                y = y.float()
+                x = self._data[index:index+self._seq]
+                y = self._data[index:index+self._seq]
                 return x, y
         
         examples = 1000
@@ -40,7 +49,7 @@ class TestTraining(unittest.TestCase):
         dataloader = DataLoader(dataset, batch_size=8, shuffle=False)
         criterion = nn.CrossEntropyLoss()
         lr = 0.001
-        n_epochs = 200
+        n_epochs = 20
         training.train(model, dataloader, criterion, lr, n_epochs=n_epochs)
     
     def test_token_train(self) -> None:
