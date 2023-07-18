516b73a7b2ab05d2959a6f16db8cdd96fffa04f0
norman-tom <tom.a.norman@gmail.com>
2023-07-18 18:29:34 +1000

    beefed up

--- tests/test_training.py
+++ tests/test_training.py
@@ -9,22 +9,13 @@ from mininlp.data import SequenceDataset
 import os
 
 class TestTraining(unittest.TestCase):
-    def test_simple_train(self) -> None:
-        max_seq = 10
-        de = 24
-        v_size = 26
-        n_heads = 4
-        N = 2
-        factor = 4
-        mask = torch.triu(torch.ones(max_seq, max_seq), diagonal=1)
-        model = DTransformer(N, de, v_size, max_seq, n_heads, factor, mask)
 
     def test_simple_train(self) -> None:
-        max_seq = 10
-        de = 24
+        max_seq = 64
+        de = 128
         v_size = 26
         n_heads = 4
-        N = 2
+        N = 4
         factor = 4
         mask = torch.triu(torch.ones(max_seq, max_seq), diagonal=1)
         model = DTransformer(N, de, v_size, max_seq, n_heads, factor, mask)
